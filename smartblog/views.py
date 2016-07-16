38e8c6c26e5a3a1af60653de135fdf32eedc9a7f
shivanXI <shivanstar7@gmail.com>
2016-07-16 10:37:56 +0530

    Security to smartblog addedgit add --all .

--- smartblog/views.py
+++ smartblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post
 
@@ -14,6 +15,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'smartblog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -27,6 +29,8 @@ def post_new(request):
         form = PostForm()
     return render(request, 'smartblog/post_edit.html', {'form': form})
 
+
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -41,15 +45,21 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'smartblog/post_edit.html', {'form': form})
 
+
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'smartblog/post_draft_list.html', {'posts': posts})
 
+
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('smartblog.views.post_detail', pk=pk)
 
+
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
